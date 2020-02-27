059f03b984414cc4c05fe27316fc8705651289b7
Yuanyuan <49631293+yf6666@users.noreply.github.com>
2020-02-27 02:48:18 -0700

    Update db.py

--- jbrowse/db.py
+++ jbrowse/db.py
@@ -23,7 +23,7 @@ except:
 #import Image
 
 
-DB_HOME = os.path.join('db', )
+DB_HOME = os.path.join('db',)
 
 #env = bsddb3.db.DBEnv()
 #env.open(
@@ -41,9 +41,7 @@ dbenv.open(DB_HOME, bsddb3.db.DB_CREATE | bsddb3.db.DB_INIT_MPOOL)
 
 env = bsddb3.db.DB(dbenv)
 
-env.open("test.log", bsddb3.db.DB_BTREE, bsddb3.db.DB_CREATE)
-
-
+env.open("test.db", bsddb3.db.DB_BTREE, bsddb3.db.DB_CREATE)
 CLOSE_ON_EXIT = []
 
 # this prevents lockers/locks from accumulating when python is closed
@@ -272,7 +270,6 @@ class Regions(Container):
         return [(k, d["min"]) for k, d in self.get()["data"].iteritems()]
 
 
-
 class ChromLengths(Resource):
     CHROM_ORDER = [str(x+1) for x in range(22)]+["X"]
     CHROM_RANK = dict(zip(CHROM_ORDER, enumerate(CHROM_ORDER)))
@@ -299,6 +296,7 @@ class ChromLengths(Resource):
             (ch, chroms[ch])
             for ch in self.CHROM_ORDER
             ])
+
 
 def get_model(probes, break_after):
     """Calculate breaks and segments after PrunedDP."""
@@ -1087,5 +1085,3 @@ class ModelError(Vector):
         models = Models(self.info["name"], self.info["chr"]).get()
         error = numpy.array([0 for m in models])
         return error
-
-
