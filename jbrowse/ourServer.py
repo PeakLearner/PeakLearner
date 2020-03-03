240b90e0d7f9563dea9d28ba117b80c8fff9500c
allen.m.clarke@gmail.com <allen.m.clarke@gmail.com>
2020-03-03 15:05:35 -0700

    fixed weird issue where server was not handling range requests after db was connected, renamed file

--- jbrowse/ourServer.py
+++ jbrowse/ourServer.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python
-'''
-    Use this in the same way as Python's SimpleHTTPServer:
-    python -m RangeHTTPServer [port]
-    The only difference from SimpleHTTPServer is that RangeHTTPServer supports
-    'Range:' headers to load portions of files. This is helpful for doing local web
-    development with genomic data files, which tend to be to large to load into the
-    browser all at once.
-    '''
 
+#These are needed to handle range requests
 import os
 import re
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+#These are needed to handle the database
 import db
+
+#these are needed to handle our post requests
 import json as simplejson
-from http.server import BaseHTTPRequestHandler, HTTPServer
 from io import BytesIO
-try:
-    # Python3
-    from http.server import SimpleHTTPRequestHandler
-
-except ImportError:
-    # Python 2
-    from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 
+PORT_NUMBER = 8081
+
+#https://github.com/danvk/RangeHTTPServer
+#see link above for original code which we copied here to properly extend
 def copy_byte_range(infile, outfile, start=None, stop=None, bufsize=16*1024):
     '''Like shutil.copyfileobj, but only copy a range of the streams.
-        Both start and stop are inclusive.
-        '''
+    Both start and stop are inclusive.
+    '''
     if start is not None: infile.seek(start)
     while 1:
         to_read = min(bufsize, stop + 1 - infile.tell() if stop else bufsize)
@@ -36,21 +31,18 @@ def copy_byte_range(infile, outfile, start=None, stop=None, bufsize=16*1024):
         outfile.write(buf)
 
 
-PORT_NUMBER = 8083
-
-
 BYTE_RANGE_RE = re.compile(r'bytes=(\d+)-(\d+)?$')
 def parse_byte_range(byte_range):
     '''Returns the two numbers in 'bytes=123-456' or throws ValueError.
-        The last number or both numbers may be None.
-        '''
+    The last number or both numbers may be None.
+    '''
     if byte_range.strip() == '':
         return None, None
-    
+
     m = BYTE_RANGE_RE.match(byte_range)
     if not m:
         raise ValueError('Invalid byte range %s' % byte_range)
-    
+
     first, last = [x and int(x) for x in m.groups()]
     if last and last < first:
         raise ValueError('Invalid byte range %s' % byte_range)
@@ -58,11 +50,57 @@ def parse_byte_range(byte_range):
 
 
 class RangeRequestHandler(SimpleHTTPRequestHandler):
-    """Adds support for HTTP 'Range' requests to SimpleHTTPRequestHandler
-        The approach is to:
-        - Override send_head to look for 'Range' and respond appropriately.
-        - Override copyfile to only transmit a range when requested.
-        """
+    def send_head(self):
+        if 'Range' not in self.headers:
+            self.range = None
+            return SimpleHTTPRequestHandler.send_head(self)
+        try:
+            self.range = parse_byte_range(self.headers['Range'])
+        except ValueError as e:
+            self.send_error(400, 'Invalid byte range')
+            return None
+        first, last = self.range
+
+        # Mirroring SimpleHTTPServer.py here
+        path = self.translate_path(self.path)
+        f = None
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except IOError:
+            self.send_error(404, 'File not found')
+            return None
+
+        fs = os.fstat(f.fileno())
+        file_len = fs[6]
+        if first >= file_len:
+            self.send_error(416, 'Requested Range Not Satisfiable')
+            return None
+
+        self.send_response(206)
+        self.send_header('Content-type', ctype)
+        self.send_header('Accept-Ranges', 'bytes')
+
+        if last is None or last >= file_len:
+            last = file_len - 1
+        response_length = last - first + 1
+
+        self.send_header('Content-Range',
+                         'bytes %s-%s/%s' % (first, last, file_len))
+        self.send_header('Content-Length', str(response_length))
+        self.send_header('Last-Modified', self.date_time_string(fs.st_mtime))
+        self.end_headers()
+        return f
+
+    def copyfile(self, source, outputfile):
+        if not self.range:
+            return SimpleHTTPRequestHandler.copyfile(self, source, outputfile)
+
+        # SimpleHTTPRequestHandler uses shutil.copyfileobj, which doesn't let
+        # you stop the copying before the end of the file.
+        start, stop = self.range  # set in send_head()
+        copy_byte_range(source, outputfile, start, stop)
+
     def do_POST(self):
         print('Got a post')
         content_length = int(self.headers['Content-Length'])
@@ -78,77 +116,21 @@ class RangeRequestHandler(SimpleHTTPRequestHandler):
         jsondata = simplejson.loads(body)
         print(jsondata)
         print(jsondata["test"])
-        #self.wfile.write(response)
-        #This is an example of how to get data out of the post request sent from the browser.
-        #This should be replaced with th code to put the request object (called jsondata) into the DB
         
         testDB = db.testDB
         testDB.put(b'test',jsondata["test"])
         print("database test")
         print(testDB.get(b'test'))
-        
-        
-def send_head(self):
-    if 'Range' not in self.headers:
-        self.range = None
-        return SimpleHTTPRequestHandler.send_head(self)
-        try:
-            self.range = parse_byte_range(self.headers['Range'])
-        except ValueError as e:
-            self.send_error(400, 'Invalid byte range')
-            return None
-        first, last = self.range
-        
-        # Mirroring SimpleHTTPServer.py here
-        path = self.translate_path(self.path)
-        f = None
-        ctype = self.guess_type(path)
-        try:
-            f = open(path, 'rb')
-        except IOError:
-            self.send_error(404, 'File not found')
-            return None
-        
-        fs = os.fstat(f.fileno())
-        file_len = fs[6]
-        if first >= file_len:
-            self.send_error(416, 'Requested Range Not Satisfiable')
-            return None
-        
-        self.send_response(206)
-        self.send_header('Content-type', ctype)
-        self.send_header('Accept-Ranges', 'bytes')
-        
-        if last is None or last >= file_len:
-            last = file_len - 1
-        response_length = last - first + 1
-        
-        self.send_header('Content-Range',
-                         'bytes %s-%s/%s' % (first, last, file_len))
-        self.send_header('Content-Length', str(response_length))
-    self.send_header('Last-Modified', self.date_time_string(fs.st_mtime))
-    self.end_headers()
-    return f
-        
-    def copyfile(self, source, outputfile):
-        if not self.range:
-            return SimpleHTTPRequestHandler.copyfile(self, source, outputfile)
-        
-        # SimpleHTTPRequestHandler uses shutil.copyfileobj, which doesn't let
-        # you stop the copying before the end of the file.
-            start, stop = self.range  # set in send_head()
-        copy_byte_range(source, outputfile, start, stop)
-
-
 
 try:
     #Create a web server and define the handler to manage the
-    #incoming request
-    server = HTTPServer(('', PORT_NUMBER), RangeRequestHandler)
+    #incoming requests
+    server = BaseHTTPServer.HTTPServer(('', PORT_NUMBER), RangeRequestHandler)
     print ('Started httpserver on port ', PORT_NUMBER)
     #Wait forever for incoming http requests
     server.serve_forever()
     
 except KeyboardInterrupt:
+    #This is the way to close the server, by hitting control + c
     print ('^C received, shutting down the web server')
     server.socket.close()
