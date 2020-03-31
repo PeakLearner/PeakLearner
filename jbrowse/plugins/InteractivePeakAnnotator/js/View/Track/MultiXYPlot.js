f68bcc6ef816a6038c6e302ae68ef14bbb388379
Jacob-D-Christiansen <Jacob.D.Christiansen@gmail.com>
2020-03-31 11:44:59 -0700

    removed submodules

--- jbrowse/plugins/InteractivePeakAnnotator/js/View/Track/MultiXYPlot.js
+++ jbrowse/plugins/InteractivePeakAnnotator/js/View/Track/MultiXYPlot.js
@@ -8,33 +8,68 @@ function (
     Util,
     XYPlot
 ) {
-    return declare([ XYPlot], {
-        _defaultConfig: function () {
-            return Util.deepUpdate(dojo.clone(this.inherited(arguments)), {
-                onHighlightClick: function (feature, track) {
-                    var features = JSON.parse(localStorage.getItem('ipaFeatures'));
-                    var states = ['unknown', 'peak', 'nopeak'];
-                    features.forEach(f => {
-                        if (f.ref === feature.get('ref') &&
-                            f.start === feature.get('start') &&
-                            f.end === feature.get('end')
-                        ) {
-                            if (f[track.name]) {
-                                f[track.name] = (f[track.name] + 1) % states.length;
-                            } else {
-                                f[track.name] = 1;
-                            }
+{
+    return declare([ XYPlot],
+     {
+        _defaultConfig: function () 
+        {
+            return Util.deepUpdate(dojo.clone(this.inherited(arguments)), 
+            {
+                onHighlightClick: function (feature, track) 
+                {
+                  var features = JSON.parse(localStorage.getItem('ipaFeatures'));
+                  var highlightFlag = parseInt(localStorage.getItem('highlightFlag'));
+                  if(highlightFlag === 1)
+                  {
+                       var states = ['unknown', 'peak', 'nopeak', 'peakStart', 'peakEnd'];
+                       features.forEach(f => 
+                       {
+                           if (f.ref === feature.get('ref') &&
+                               f.start === feature.get('start') &&
+                               f.end === feature.get('end')) 
+                           {
+                               if (f[track.name]) 
+                               {
+                                   f[track.name] = (f[track.name] + 1) % states.length;
+                               } 
+                               else 
+                               {
+                                   f[track.name] = 1;
+                               }
+                           }
+                       });
+                   }
+                   else
+                   {
+                     features = features.filter(function (f)
+                     {
+                        if (f.start !== feature.get('start')) 
+                        {
+                           return f;
                         }
-                    });
-                    localStorage.setItem('ipaFeatures', JSON.stringify(features));
-                    track.redraw();
+                     });
+                     var removeJSON = {
+                        'name' : track.name,
+                        'ref' : feature.get('ref'),
+                        'start' : feature.get('start'),
+                        'end' : feature.get('end')
+                     }
+                     console.log("removing label: ", removeJSON);
+                     //sendPost(removeJSON);
+                   }
+                   track.redraw();
+                   localStorage.setItem('ipaFeatures', JSON.stringify(features));
                 },
-                highlightColor: function (feature, track) {
-                    var states = {0: '#f005', 1: '#0f05', 2: '#ff05'};
+                
+                highlightColor: function (feature, track) 
+                {
+                    var states = {0: 'rgba(100,100,100,.4)', 1: '#0f05', 2: '#ff05', 3: 'rgba(255,0,0,.4)', 4: 'rgba(255,150,0,.4)'};
                     return states[feature.get(track.name) || 0];
                 },
-                indicatorColor: function (feature, track) {
-                    var states = {0: '#f00', 1: '#0f0', 2: '#ff0'};
+                
+                indicatorColor: function (feature, track) 
+                {
+                    var states = {0: '#f00', 1: '#0f0', 2: '#ff0', 3: '#d3034f', 4: '#d30303'};
                     return states[feature.get(track.name) || 0];
                 }
             });
--- jbrowse/plugins/InteractivePeakAnnotator/js/main.js
+++ jbrowse/plugins/InteractivePeakAnnotator/js/main.js
@@ -10,13 +10,20 @@ function (
         constructor: function (args) {
             console.log('InteractivePeakAnnotator plugin starting');
             dojo.subscribe('/jbrowse/v1/n/globalHighlightChanged', function (data) {
-                console.log(data);
                 if (data.length) {
+                    localStorage.setItem('highlightFlag', 1);
                     const region = data[0];
                     const regions = JSON.parse(localStorage.getItem('ipaFeatures') || '[]');
                     regions.push(region);
                     localStorage.setItem('ipaFeatures', JSON.stringify(regions));
-                    args.browser.clearHighlight();
+                    //args.browser.clearHighlight();
+                }
+                else
+                {
+                  localStorage.setItem('highlightFlag', 0);
+                  var labelsJSON = JSON.parse(localStorage.getItem('ipaFeatures'));
+                  console.log("sending new labels: ", labelsJSON[labelsJSON.length - 1]);
+                  //sendPost(labeslJSON);
                 }
             });
         }
