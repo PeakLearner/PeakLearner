6301d5cd30080f427806fde7aee3bd580774201e
John Jackson <jcj52@nau.edu>
2020-04-14 11:21:09 -0700

    formatted as python unittest

--- jbrowse/tests/cluster_tests/clusterTest.py
+++ jbrowse/tests/cluster_tests/clusterTest.py
@@ -1,25 +1,26 @@
-#!usr/bin/env python
+#!/usr/bin/python
+
+import unittest
 import subprocess
 
-#This next block is our simulation of the cluster and using Dr. Hockings R code
-############################################
+class ClusterTest(unittest.TestCase):
+    def testCreateModel(self):
+        #This next block is our simulation of the cluster and using Dr. Hockings R code
+        ############################################
 
-# Define command and arguments
-command = 'Rscript'
-path2script = '../PeakSegDisk-master/R/PeakSegFPOP_dir.R'
+        # Define command and arguments
+        command = 'Rscript'
+        path2script = '../PeakSegDisk-master/R/PeakSegFPOP_dir.R'
 
-# the function we want to run has 2 arguments
-#a path to the coverage data we are observing
-#penalty > 0
-args = ['../PeakLearner-1.1/jbrowse/tests/data/all_labels.bigBed', '1']
+        # the function we want to run has 2 arguments
+        #a path to the coverage data we are observing
+        #penalty > 0
+        args = ['../PeakLearner-1.1/jbrowse/tests/data/all_labels.bigBed', '1']
 
-# Build subprocess command
-cmd = [command, path2script] + args
+        # Build subprocess command
+        cmd = [command, path2script] + args
 
-# check_output will run the command and store to result
-newModel = subprocess.check_output(cmd, universal_newlines=True)
+        # check_output will run the command and store to result
+        newModel = subprocess.check_output(cmd, universal_newlines=True)
 
-print('The new model we got:', newModel)
-############################################
-#For now, in this section we will always send back a specific model which we will write to here
-#this should make jbrowse redraw when it gets a response to view the model
+        assertIsNotNone(newModel)
