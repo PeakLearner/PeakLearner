0b2e4cb7eef9e75a445c76dd3564d1c21c43dfd3
bradbarber64 <60404970+bradbarber64@users.noreply.github.com>
2021-03-05 16:31:45 -0700

    Add files via upload

---
+++ tests/authUnitTest.py
@@ -0,0 +1,26 @@
+import unittest
+from pyramid import testing
+
+class AuthUnitTests(unittest.TestCase):
+	user = USERS.get(userid)
+	
+	def printResults:
+		print("User is user?")
+		assert user == userid
+		#to change to uuid
+		
+		print("User is not in database?")
+		#make sure client is sent an error message
+		#check with uuid
+		
+		print("Google auth passes the password?")
+		#send error if not
+		#allow login if so
+		
+		print("User is able to access hubs?")
+		#might need to be done later with permissions
+		
+		print("Error page is accessible?")
+		#insert assert
+		
+		
