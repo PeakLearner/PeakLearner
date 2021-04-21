be698dd40b5cee8e133c679a272789a5352c00ac
zacharywl123 <zsw23@nau.edu>
2021-04-21 16:03:23 -0700

    js documentation

--- api/static/myHubs.js
+++ api/static/myHubs.js
@@ -1,76 +1,107 @@
-function hubInfoAlert() {
-    window.open("/about/");
-}
+//Defaults the show_owned and show_shared check items to true on first page load.
 sessionStorage.setItem('show_owned', 'true');
 sessionStorage.setItem('show_shared', 'true');
 
-// keep scroll position on refresh
+
+/**
+ * Handles refreshes of myHubs page
+ */
 document.addEventListener("DOMContentLoaded", function(event) {
+
+    // check session to see if edit hubs button selected, or show owned/shared hubs filters are checked
     let showEdit = (sessionStorage.getItem('show_edit') === 'true');
     let showOwned = (sessionStorage.getItem('show_owned') === 'true');
     let showShared = (sessionStorage.getItem('show_shared') === 'true');
+    let scrollPosition = localStorage.getItem('scrollPosition');
 
+    // access filter check boxes and uncheck them visually if unchecked before refresh
     let myHubsCheck = document.getElementById('owned-hubs');
     if (!showOwned) {
-
         myHubsCheck.checked = false;
     }
-
     let sharedHubsCheck = document.getElementById('shared-hubs');
     if (!showShared) {
         sharedHubsCheck.checked = false;
     }
 
+    // based on session storage call edit hubs to show are not show hub editing UI features
     editHubs(showEdit);
+
+    // same as edit hubs but hides or un-hides hubs based on filter checks
     checkHubs();
 
-    let scrollPosition = localStorage.getItem('scrollPosition');
+    // resets scroll position if a previous storage item found
     if (scrollPosition) window.scrollTo(0, scrollPosition);
 });
+
+/**
+ * Saves current scroll position before refresh
+ */
 window.onbeforeunload = function(e) {
     localStorage.setItem('scrollPosition', window.scrollY);
 };
 
+/**
+ * Changes the visibility of a hub depending on the checkbox
+ */
 function checkHubs() {
+
+    // gets the current status of the checkboxes owned-hubs and shared-hubs to determine visibility
     let myHubsCheck = document.getElementById('owned-hubs');
     let sharedHubsCheck = document.getElementById('shared-hubs');
 
+    // gets all hubs on my hubs page
     const myHubs = document.getElementsByClassName("my-hubs");
-    if (myHubsCheck.checked) {
-        for (let hub = 0; hub < myHubs.length; hub++) {
-            myHubs[hub].style.display = "block";
-            if(myHubs[hub].id === "hubs-break"){
-                myHubs[hub].style.display = "inline"
+    const sharedHubs = document.getElementsByClassName("shared-hubs");
+
+    // handles visibility of hubs
+    changeVisibility(myHubsCheck, myHubs)
+    changeVisibility(sharedHubsCheck, sharedHubs)
+}
+
+/**
+ * Handles the visibility of hubs depending on the filter checked for the class of the hub. (Owned or Shared)
+ *
+ * @param check - checkbox relating to the type of hub
+ * @param hubs - collection of hubs of a certain class type (owned or shared)
+ */
+function changeVisibility(check, hubs) {
+
+    // if filter checked then show hubs
+    if (check.checked) {
+        for (let hub = 0; hub < hubs.length; hub++) {
+            hubs[hub].style.display = "block";
+            if(hubs[hub].id === "hubs-break"){
+                hubs[hub].style.display = "inline"
             }
         }
+
+        // set session check to retain filter on refresh
         sessionStorage.setItem('show_owned', 'true');
-    } else {
-        for (let hub = 0; hub < myHubs.length; hub++) {
-            myHubs[hub].style.display = "none";
-        }
-        sessionStorage.setItem('show_owned', 'false');
     }
 
-    const sharedHubs = document.getElementsByClassName("shared-hubs");
-    if (sharedHubsCheck.checked) {
-        for (let hub = 0; hub < sharedHubs.length; hub++) {
-            sharedHubs[hub].style.display = "block";
-            if(sharedHubs[hub].id === "hubs-break"){
-                sharedHubs[hub].style.display = "inline"
-            }
+    // if filter unchecked hide hubs
+    else {
+        for (let hub = 0; hub < hubs.length; hub++) {
+            hubs[hub].style.display = "none";
         }
-        sessionStorage.setItem('show_shared', 'true');
-    } else {
-        for (let hub = 0; hub < sharedHubs.length; hub++) {
-            sharedHubs[hub].style.display = "none";
-        }
-        sessionStorage.setItem('show_shared', 'false');
+
+        // set session check to retain filter on refresh
+        sessionStorage.setItem('show_owned', 'false');
     }
 }
 
+/**
+ * Determines whether elements relating to hub editing are shown or not depending on param value
+ *
+ * @param show - boolean value determining whether editing elements are shown (True - shown, False - hidden).
+ */
 function editHubs(show) {
+
+    // gets all the editing elements (including add user form, delete buttons, etc.)
     let editElements = document.getElementsByClassName("edit-elements");
 
+    // show editing elements
     if (show) {
         document.getElementById("stop-edit").style.display = "block";
         document.getElementById("edit").style.display = "none";
@@ -79,9 +110,12 @@ function editHubs(show) {
             editElements[element].style.display = "inline";
         }
 
+        // store option of showing editing elements
         sessionStorage.setItem('show_edit', 'true');
-        showEdit = true;
-    } else {
+    }
+
+    // hide editing elements
+    else {
         document.getElementById("stop-edit").style.display = "none";
         document.getElementById("edit").style.display = "block";
 
@@ -89,7 +123,8 @@ function editHubs(show) {
             editElements[element].style.display = "none";
         }
 
+        // store option of hiding editing elements
         sessionStorage.setItem('show_edit', 'false');
-        showEdit = false;
     }
+
 }
