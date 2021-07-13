0ad782dd2eda17305522ee538dd2121df525b58e
Tristan Miller <tem83@nau.edu>
2021-07-13 13:41:22 -0700

    Remove debug statements

--- core/Jobs/Jobs.py
+++ core/Jobs/Jobs.py
@@ -121,7 +121,6 @@ class Job(metaclass=JobType):
         while current is not None:
             key, job = current
             if self.equals(job):
-                print(job.debugDict())
                 cursor.close()
                 return True
             current = cursor.next()
@@ -240,30 +239,6 @@ class Job(metaclass=JobType):
                   'status': self.status,
                   'id': self.id,
                   'iteration': self.iteration,
-                  'tasks': self.tasks,
-                  'trackUrl': self.trackUrl,
-                  'priority': int(self.priority)}
-
-        try:
-            output['lastModified'] = self.lastModified
-        except AttributeError:
-            pass
-
-        if self.status.lower() == 'done':
-            try:
-                output['time'] = self.time
-            except AttributeError:
-                pass
-
-        return output
-
-    def debugDict(self):
-        output = {'user': self.user,
-                  'hub': self.hub,
-                  'track': self.track,
-                  'problem': self.problem,
-                  'jobType': self.jobType,
-                  'status': self.status,
                   'tasks': self.tasks,
                   'trackUrl': self.trackUrl,
                   'priority': int(self.priority)}
@@ -759,14 +734,10 @@ def spawnJobs(data, txn=None):
 
     jobsLeft = cfg.maxJobsToSpawn - len(jobs)
 
-    print('jobsLeft', jobsLeft)
-
     if jobsLeft > 0:
         resOut = getResolutionJobs(jobsLeft, txn=None)
         if resOut is not None and len(resOut) > 0:
             jobs.extend(resOut)
-
-    print('numJobs', len(jobs))
 
     for job in jobs:
         job.putNewJob(txn=txn, checkExists=False)
@@ -803,7 +774,6 @@ def getPotentialJobs(contigJobs, txn=None):
                 continue
 
             if not jobToAppend.checkIfExists(txn=txn):
-                print(jobToAppend.debugDict())
                 output.append(jobToAppend)
             continue
 
@@ -898,7 +868,7 @@ def jobToRefine(key, modelSums, txn=None):
                                      problem['chromStart']).get(txn=txn)
 
             if iteration < 5:
-                return submitGridSearch(problem, data, minPenalty, maxPenalty, regions, txn=txn)
+                return submitGridSearch(problem, data, minPenalty, maxPenalty, regions)
         return
 
     elif numMinErrors == 1:
@@ -953,7 +923,7 @@ def submitOOMJob(problem, data, penalty, jobType, regions):
                           regions)
 
 
-def submitGridSearch(problem, data, minPenalty, maxPenalty, regions, num=cfg.gridSearchSize, txn=None):
+def submitGridSearch(problem, data, minPenalty, maxPenalty, regions, num=cfg.gridSearchSize):
     minPenalty = float(minPenalty)
     maxPenalty = float(maxPenalty)
     penalties = np.linspace(minPenalty, maxPenalty, num + 2).tolist()[1:-1]
